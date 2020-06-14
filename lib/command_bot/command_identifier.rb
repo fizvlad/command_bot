abb8b9118951f939fa19f5dff363429f2784a06d
Fizvlad <fizvlad@mail.ru>
2020-06-14 16:04:38 +0300

    Add default command initalizer

--- lib/command_bot/command_identifier.rb
+++ lib/command_bot/command_identifier.rb
@@ -21,15 +21,52 @@ module CommandBot
 
     # @param [String] command prefix.
     # @return [CommandIdentifier] default identifier with prefix.
-    def self.default(prefix: '')
-      self.new do |bot, text, data|
-        # TODO
+    def self.default(prefix: '', disallow_whitespace: false, o_prefix: '--', o_separator: '=')
+      new do |bot, text_original, data|
+        command_call = CommandCall.new(bot, text_original, data)
+        text = text_original.dup
+
+        # Not a command if got prefix and text do not start with it
+        next nil unless prefix.empty? || text.delete_prefix!(prefix)
+        # Check for whitespace
+        next nil if disallow_whitespace && text.lstrip!
+
+        words = text.split
+        # Name
+        command_name = words.shift
+        next nil if command_name.nil? # Just a prefix
+        command_name.downcase! # Usually commands should not depend on case, so downcasing
+        # Options
+        options = {}
+        while words.first&.start_with? o_prefix
+          o_str = words.shift
+          o_str.downcase!
+          o_str.delete_prefix! o_prefix
+
+          # Need to check for splitter in key
+          o_key, *, o_value = o_str.partition(o_separator)
+
+          # Following block allows writing '!command --key = value'
+          o_value = nil if o_value.empty?
+          o_value ||= if words.first == o_separator
+                        *, o_value = words.shift 2
+                        o_value || ''
+                      else
+                        ''
+                      end
+          options[o_key] = o_value
+        end
+
+        command_call.command = bot.find_command(command_name)
+        command_call.arguments = words # Everything left is considered to be arguments
+        command_call.options = options
+        command_call
       end
     end
 
     # @return [CommandIdentifier] primitive optionless identifier.
     def self.primitive
-      self.new do |bot, text, data|
+      new do |bot, text, data|
         command_call = CommandCall.new(bot, text, data)
         words = text.split
         command_call.command = bot.find_command(words.shift || '')
